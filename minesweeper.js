feaebfd8fcfcac7e9e0a4cb8823cb360e59ea50f
yuu200219 <85803667+yuu200219@users.noreply.github.com>
2022-09-01 00:39:27 +0800

    Add files via upload

---
+++ minesweeper.js
@@ -0,0 +1,189 @@
+//for logic
+//switch button: reveal/flag
+// check win/lose
+export const BOARD_SIZE_WIDTH = 10
+export const BOARD_SIZE_HEIGHT = 15
+export const NUMBER_OF_MINES = 20
+
+export const GAME_STATUS = {
+    WIN: 'win',
+    LOSE: 'lose'
+}
+
+export function checkWin(board, mainTitle) {
+    return board.every(row => {
+        return row.every(tile => {
+            return tile.status === TILE_STATUS.NUMBER || 
+                    (tile.mine && tile.status === TILE_STATUS.FLAG)
+        })
+    })
+}
+
+export function checkLose(tile, mainTitle) {
+    if(tile.status == TILE_STATUS.MINE) {
+        mainTitle.dataset.game = GAME_STATUS.LOSE
+        return true
+    }
+    else return false
+}
+
+export const TILE_STATUS = {
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    FLAG: 'flag'
+}
+export const OPTION_STATUS = {
+    MINE: 'mine',
+    FLAG: 'flag'
+}
+// export const LEVEL_STATUS = {
+//     EASY: 'easy',
+//     MEDIUM: 'medium',
+//     HARD: 'hard'
+// }
+
+// export function Level(level) {
+//     level.dataset.level = LEVEL_STATUS.EASY
+//     BOARD_SIZE_WIDTH = 10
+//     BOARD_SIZE_HEIGHT = 10
+//     NUMBER_OF_MINES = 10
+// }
+// export function LevelSwitch(level) {
+//     if(level.dataset.level === LEVEL_STATUS.EASY) {
+//         BOARD_SIZE_WIDTH = 10
+//         BOARD_SIZE_HEIGHT = 10
+//         NUMBER_OF_MINES = 10
+//     }
+//     else if(level.dataset.level === LEVEL_STATUS.MEDIUM) {
+//         BOARD_SIZE_WIDTH = 10
+//         BOARD_SIZE_HEIGHT = 13
+//         NUMBER_OF_MINES = 15
+//     }
+//     else if(level.dataset.level === LEVEL_STATUS.HARD) {
+//         BOARD_SIZE_WIDTH = 10
+//         BOARD_SIZE_HEIGHT = 16
+//         NUMBER_OF_MINES = 25
+//     }
+// }
+
+export function createBoard(boardSize_x, boardSize_y, numberOfMine) {
+    const board = []
+    const minePosition = getMinePosition(boardSize_x, boardSize_y, numberOfMine)
+    // console.log(numberOfMine)
+    console.log(minePosition)
+
+    for (let x = 0; x < boardSize_x; x++) {
+        const row = []
+        for (let y = 0; y < boardSize_y; y++) {
+
+            const element = document.createElement("div")
+            element.dataset.status = TILE_STATUS.HIDDEN
+
+            const tile = {
+                element,
+                x,
+                y,
+                mine: minePosition.some(positionOverFlow.bind(null, {x, y})),
+                get status() {
+                    return element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
+            }
+            row.push(tile)
+        }
+        board.push(row)
+    }
+    return board
+}
+
+export function Option(opt) {
+    opt.dataset.opt = OPTION_STATUS.MINE //default is mine
+}
+export function OptionSwitch(opt) {
+    // if(opt.dataset.opt !== OPTION_STATUS.MINE && opt.dataset.opt !== OPTION_STATUS.FLAG ) {
+    //     opt.dataset.opt = OPTION_STATUS.MINE
+    // }
+    if(opt.dataset.opt === OPTION_STATUS.MINE) {
+        opt.dataset.opt = OPTION_STATUS.FLAG
+    }
+    else if(opt.dataset.opt === OPTION_STATUS.FLAG) {
+        opt.dataset.opt = OPTION_STATUS.MINE
+    }
+}
+
+export function flagedTile(tile) {
+    if(tile.status !== TILE_STATUS.HIDDEN && tile.status !== TILE_STATUS.FLAG) {
+        return
+    }
+    if(tile.status === TILE_STATUS.FLAG) {
+        tile.status = TILE_STATUS.HIDDEN
+    }
+    else {
+        tile.status = TILE_STATUS.FLAG
+    }
+}
+
+export function revealTile(board, tile) {
+    console.log(tile)
+    if(tile.status !== TILE_STATUS.HIDDEN) {
+        // console.log("return")
+        return
+    }
+    if(tile.mine) {
+        tile.status = TILE_STATUS.MINE
+        return
+    }
+
+    tile.status = TILE_STATUS.NUMBER
+    const adjacentTiles = nearbyTile(board, tile)
+    const mines = adjacentTiles.filter(t => t.mine)
+    if(mines.length === 0) {
+        adjacentTiles.forEach(revealTile.bind(null, board))
+    }
+    else {
+        console.log(mines.length)
+        tile.element.textContent = mines.length
+    }
+    // console.log(adjacentTiles)
+}
+
+function getMinePosition(boardSize_x, boardSize_y, numberOfMine) {
+    const positions = []
+    while(positions.length < numberOfMine) {
+        const position = {
+            x: randomNumber(boardSize_x),
+            y: randomNumber(boardSize_y)
+        }
+        if(!positions.some(p => positionOverFlow(p, position))) {
+            positions.push(position)
+        }
+    }
+    return positions
+}
+
+function positionOverFlow(a, b) {
+    return a.x == b.x && a.y == b.y
+}
+
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+}
+
+function nearbyTile(board, {x, y}) {
+    const tiles = []
+
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            if(xOffset == 0 && yOffset == 0) continue
+            const tile = board[x + xOffset]?.[y + yOffset]
+            if(tile) {
+                tiles.push(tile)
+            }
+            
+        }
+    }
+    return tiles
+}
---
+++ script.js
@@ -0,0 +1,148 @@
+//for display UI
+import { createBoard, flagedTile, Option, OptionSwitch, OPTION_STATUS, TILE_STATUS,
+    revealTile,
+    checkWin, checkLose,
+    GAME_STATUS,
+    BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES} from "./minesweeper.js";
+
+// const levelElement = document.querySelector('.level')
+// Level(levelElement)
+
+// const easy = document.querySelector('.easy')
+// const medium = document.querySelector('.medium')
+// const hard = document.querySelector('.hard')
+const board = createBoard(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES)
+const boardElement = document.querySelector('.board')
+const mineLeftText = document.querySelector('[data-mine-count]')
+const mainTitle = document.querySelector('.main-title')
+const optElement = document.querySelector('.option')
+
+mainTitle.textContent = "Minesweeper"
+
+// easy.addEventListener("click", () => {
+//     console.log("easy")
+    
+//     levelElement.dataset.level = LEVEL_STATUS.EASY
+//     LevelSwitch(levelElement)
+
+//     // board = createBoard(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES)
+//     // listMineLeft()
+//     boardDetector(board)
+// })
+// medium.addEventListener("click", () => {
+//     console.log("medium")
+    
+//     levelElement.dataset.level = LEVEL_STATUS.MEDIUM
+//     LevelSwitch(levelElement)
+
+//     // board = createBoard(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES)
+//     // listMineLeft()
+//     boardDetector(board)
+// })
+// hard.addEventListener("click", () => {
+//     console.log("hard")
+    
+//     levelElement.dataset.level = LEVEL_STATUS.HARD
+//     LevelSwitch(levelElement)
+
+//     // board = createBoard(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES)
+//     // listMineLeft()
+//     boardDetector(board)
+// })
+
+Option(optElement)
+optElement.addEventListener("click", () => {
+    OptionSwitch(optElement)
+    // console.log(optElement.dataset.opt)
+})
+// function boardDetector(board) {
+//     // boardElement.style.setProperty("--size_x", BOARD_SIZE_WIDTH)
+//     // boardElement.style.setProperty("--size_y", BOARD_SIZE_HEIGHT)
+//     console.log(x)
+//     board = createBoard(BOARD_SIZE_WIDTH, BOARD_SIZE_HEIGHT, NUMBER_OF_MINES)
+//     board.forEach(row => {
+//         row.forEach(tile => {
+//             boardElement.append(tile.element)
+//             tile.element.addEventListener("click", () => {
+//                 if(optElement.dataset.opt === OPTION_STATUS.FLAG) {
+//                     flagedTile(tile)
+//                     listMineLeft()
+//                     // console.log("option status = flag")
+//                 }
+//                 else if(optElement.dataset.opt === OPTION_STATUS.MINE) {
+//                     revealTile(board, tile)
+//                 }
+//                 else {
+//                     return
+//                 }
+//                 checkGameEnd(board, tile)
+//                 // console.log("tile.element" + tile.element.dataset.status)
+//             })
+//         })
+//     })
+// }
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+        tile.element.addEventListener("click", () => {
+            if(optElement.dataset.opt === OPTION_STATUS.FLAG) {
+                flagedTile(tile)
+                listMineLeft()
+                // console.log("option status = flag")
+            }
+            else if(optElement.dataset.opt === OPTION_STATUS.MINE) {
+                revealTile(board, tile)
+            }
+            else {
+                return
+            }
+            checkGameEnd(board, tile)
+            // console.log("tile.element" + tile.element.dataset.status)
+        })
+    })
+})
+
+boardElement.style.setProperty("--size_x", BOARD_SIZE_WIDTH)
+boardElement.style.setProperty("--size_y", BOARD_SIZE_HEIGHT)
+mineLeftText.textContent = NUMBER_OF_MINES
+
+// console.log("test" + optElement.dataset.opt)
+// console.log(board)
+// function reset(board) {
+//     board.forEach(row => {
+//         row.forEach( tile => {
+
+//         })
+//     })
+// }
+
+function listMineLeft() {
+    const flagedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUS.FLAG).length
+    }, 0)
+
+    mineLeftText.textContent = NUMBER_OF_MINES - flagedTilesCount
+    // console.log("mine left: " + mineLeftText.textContent)
+}
+
+function checkGameEnd(board, tile) {
+    const win = checkWin(board, mainTitle)
+    const lose = checkLose(tile, mainTitle)
+    if(lose) {
+        // flagedTile(tile)
+        mainTitle.textContent = "You Lose!"
+    }
+    else if(win) {
+        mainTitle.dataset.game = GAME_STATUS.WIN
+        mainTitle.textContent = "You Win!"
+    }
+    if(win || lose) {
+        boardElement.addEventListener('click', stopProp, {capture: true})
+    }
+}
+
+function stopProp(e) {
+    e.stopImmediatePropagation()
+}
+// console.log(board)
