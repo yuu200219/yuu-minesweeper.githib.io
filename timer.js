8828a62d631886409ec66a2f62a6e6a76568782f
yuu200219 <w200219200219@gmail.com>
2022-09-01 23:16:46 +0800

    time edit

--- timer.js
+++ timer.js
@@ -2,7 +2,12 @@ var sec = 0;
 function pad(val) {
     return val > 9 ? val : "0" + val;
 }
+const Minutes = document.getElementById('minutes');
+const Seconds = document.getElementById('seconds');
+Minutes.textContent = "00";
+Seconds.textContent = "00";
+
 export const timer = setInterval( function() {
-    document.getElementById('seconds').innerHTML = pad(++sec%60);
-    document.getElementById('minutes').innerHTML = pad(parseInt(sec/60, 10));
+    Seconds.innerHTML = pad(++sec%60);
+    Minutes.innerHTML = pad(parseInt(sec/60, 10));
 }, 1000);
